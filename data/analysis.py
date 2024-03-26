35aa0692e81e433bb9719fded7290c7853fa7504
Rohan Lekhwani <rohanlekhwani@ymail.com>
2024-03-25 21:10:18 -0400

    feat: add average token count analysis per publication

--- data/analysis.py
+++ data/analysis.py
@@ -7,15 +7,17 @@ def get_publications_dict(domain):
     with open(f'v2/non_parallel/{domain}/data.000000000000.jsonl') as f:
         data = f.readlines()
         for line in data:
-            domain_name = urlparse(json.loads(line)['url']).netloc.split('.')[-2]
+            jsonline = json.loads(line)
+            domain_name = urlparse(jsonline['url']).netloc.split('.')[-2]
             if domain_name not in pub_dict.keys():
-                pub_dict[domain_name] = 1
+                pub_dict[domain_name] = {'count': 1, 'avg_tokens': len(jsonline['text'])}
             else:
-                pub_dict[domain_name] += 1
+                pub_dict[domain_name]['count'] += 1
+                pub_dict[domain_name]['avg_tokens'] = (pub_dict[domain_name]['avg_tokens'] * (pub_dict[domain_name]['count'] - 1) + len(jsonline['text'])) / pub_dict[domain_name]['count']
     return pub_dict
 
 def bar_plot(data_dict, xlabel, ylabel, title, save_fig):
-    plt.bar(data_dict.keys(), data_dict.values())
+    plt.bar(data_dict.keys(), [val['count'] for val in data_dict.values()])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
@@ -32,7 +34,7 @@ for d in domains:
     print(f'Publication dict for {d}:', pub_dict)
     bar_plot(pub_dict, 'Publications', 'Number of Articles', f'Distribution of Articles per Publication ({d})', f'{d}_distribution.png')
     
-    domain_dict[d] = sum(pub_dict.values())
+    domain_dict[d] = {'count': sum([val['count'] for val in pub_dict.values()])}
 
 print('Domain dict: ', domain_dict)
 bar_plot(domain_dict, 'Domain', 'Number of Articles', 'Distribution of Articles per Domain', 'domain_distribution.png')
