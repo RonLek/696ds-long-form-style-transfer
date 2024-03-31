18e0378622b9bf871aa27d83e022e4fad3d557f0
Rohan Lekhwani <rohanlekhwani@ymail.com>
2024-03-31 18:52:15 -0400

    feat: add text length distribution analysis

--- data/analysis.py
+++ data/analysis.py
@@ -1,6 +1,7 @@
 import json
 from urllib.parse import urlparse
 import matplotlib.pyplot as plt
+import numpy as np
 
 def get_publications_dict(domain):
     pub_dict = {}
@@ -15,6 +16,30 @@ def get_publications_dict(domain):
                 pub_dict[domain_name]['count'] += 1
                 pub_dict[domain_name]['avg_tokens'] = (pub_dict[domain_name]['avg_tokens'] * (pub_dict[domain_name]['count'] - 1) + len(jsonline['text'])) / pub_dict[domain_name]['count']
     return pub_dict
+
+def get_text_length_distribution(domain):
+    text_list = []
+    with open(f'v2/non_parallel/{domain}/data.000000000000.jsonl') as f:
+        data = f.readlines()
+        for line in data:
+            text_list.append(len(json.loads(line)['text']))
+   
+    text_list = [l for l in text_list if l < 10000]
+    # Define bins
+    bin_edges = np.arange(0, max(text_list) + 100, 50)  # Adjust the bin width as needed
+
+    # Compute histogram
+    hist, _ = np.histogram(text_list, bins=bin_edges)
+
+    # Plotting
+    plt.bar(bin_edges[:-1], hist, width=100)  # Use width=10 to match bin width
+    plt.title('Distribution of Text Lengths')
+    plt.xlabel('Text Length')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+    plt.xticks(np.arange(0, max(text_list) + 1, 1000))  # Adjust step size as needed
+    plt.savefig(f'{domain}_length_distribution.png')
+
 
 def bar_plot(data_dict, xlabel, ylabel, title, save_fig):
     plt.bar(data_dict.keys(), [val['count'] for val in data_dict.values()])
@@ -35,6 +60,8 @@ for d in domains:
     bar_plot(pub_dict, 'Publications', 'Number of Articles', f'Distribution of Articles per Publication ({d})', f'{d}_distribution.png')
     
     domain_dict[d] = {'count': sum([val['count'] for val in pub_dict.values()])}
+
+    get_text_length_distribution(d)
 
 print('Domain dict: ', domain_dict)
 bar_plot(domain_dict, 'Domain', 'Number of Articles', 'Distribution of Articles per Domain', 'domain_distribution.png')
